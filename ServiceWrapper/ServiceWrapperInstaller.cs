a12a67ce9edb56e164ab530b9404db72326b72fa
Alexander Savvas <a.savvas@pamediakopes.gr>
2015-01-13 17:40:47 +0200

    updated to latest log4net setting has type value fixed installer which would not read the app.config for the correct values.

--- ServiceWrapper/ServiceWrapperInstaller.cs
+++ ServiceWrapper/ServiceWrapperInstaller.cs
@@ -1,37 +1,52 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Configuration;
 using System.Configuration.Install;
+using System.Reflection;
 using System.ServiceProcess;
-using ServiceWrapper.Properties;
 
 namespace ServiceWrapper
 {
 	[RunInstaller(true)]
 	public class ServiceWrapperInstaller : Installer
 	{
-		protected static IServiceInstallHelper InstallHelper = new ServiceInstallHelper(false);
-
 		public ServiceWrapperInstaller()
 		{
+            ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+		    string exeConfigFilename = Assembly.GetAssembly(typeof (ServiceWrapperInstaller)).Location + ".config";
+		    fileMap.ExeConfigFilename = exeConfigFilename;
+		    var config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+		        //ConfigurationManager.OpenExeConfiguration(Assembly.GetAssembly(typeof (ServiceWrapperInstaller)).Location);
+            if (!config.HasFile)
+                throw new Exception("File not found at " + exeConfigFilename);
+
+		    string propertiesSettings = "applicationSettings/ServiceWrapper.Properties.Settings";
+		    ConfigurationSection configurationSection = config.GetSection(propertiesSettings);
+		    if (configurationSection == null)
+		    {
+                throw new Exception("could not find section " + propertiesSettings);
+		    }
+		    var settings = ((ClientSettingsSection)configurationSection).Settings;
+		    var getValue = new Func<string, string>((string k) => settings.Get(k).Value.ValueXml.InnerText);
+            
+            
 			var processInstaller = new ServiceProcessInstaller
 			{
-				Account = (ServiceAccount) 
-					Enum.Parse(typeof (ServiceAccount), Settings.Default.ServiceAccount),
-				Username = Settings.Default.AccountUsername == string.Empty
+                Account = (ServiceAccount)Enum.Parse(typeof(ServiceAccount), getValue("ServiceAccount"), false),
+                Username = getValue("AccountUsername") == string.Empty
 					? null
-					: Settings.Default.AccountUsername,
-				Password = Settings.Default.AccountPassword == string.Empty
+                    : getValue("AccountUsername"),
+                Password = getValue("AccountPassword") == string.Empty
 					? null
-					: Settings.Default.AccountPassword
+                    : getValue("AccountPassword")
 			};
 
 			var installer = new ServiceInstaller
 			{
-				DisplayName = InstallHelper.ServiceName,
-				StartType = (ServiceStartMode)
-					Enum.Parse(typeof(ServiceStartMode), Settings.Default.ServiceStartMode),
-				ServiceName = InstallHelper.ServiceName,
-				Description = Settings.Default.ServiceDescription
+                DisplayName = getValue("ServiceName"),
+                StartType = (ServiceStartMode)Enum.Parse(typeof(ServiceStartMode), getValue("ServiceStartMode"), false),
+                ServiceName = getValue("ServiceName"),
+				Description = getValue("ServiceDescription")
 			};
 
 			Installers.Add(processInstaller);
